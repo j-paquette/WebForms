ef0f087e196cea8964b5eae262deeae73fd2022c
j-paquette <56076749+j-paquette@users.noreply.github.com>
2022-05-16 16:12:17 -0400

    still wip, need to convert data to json and send thru api

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,20 @@
 //To enter mor than one email address, when more than one contact email is needed.
-function onMainFormSubmit(ev){
-  const csdname = document.getElementById("csdname");
+function onMainFormSubmit(event){
+  //const csdname = document.getElementById("csdname");
   //returns the input object
-  alert(csdname.value);
+  //alert(csdname.value);
+
+  event.preventDefault();
+
+  const data = new FormData(event.target);
+  //Get all the values from the form, but doesn't include input for multiple values such as checkboxes
+  const value = Object.fromEntries(data.entries());
+
+  //The Object contains an array in topics that contains all the checked values
+  value.ewsServices = data.getAll("ewsServices");
+    
+  console.log({value});
+
   //to prevent form from submitting (refreshing page)
   //go through each field and read the data from form, to build a string
   return false;
@@ -12,15 +24,18 @@ function onMainFormSubmit(ev){
 function hideExpiryDateOnChange(obj){
 if (obj.value == "yes"){
   document.getElementById("hiddenField").hidden = false;
+  //add require to expiryDate input
 }
 else
   document.getElementById("hiddenField").hidden = true;
 }
 
+//const mainForm=document.getElementById("mainForm");
+//console.log(mainForm);
+//mainForm.onsubmit=onMainFormSubmit;
 
-const mainForm=document.getElementById("mainForm");
+const form = document.querySelector('form');
 
-console.log(mainForm);
+form.addEventListener('submit', onMainFormSubmit);
 
 
-mainForm.onsubmit=onMainFormSubmit;
